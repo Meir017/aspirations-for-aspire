16c1147e817cf61b0dc8d8dce6a8e623841d38b2
Meir Blachman <meirblachman@gmail.com>
2025-11-06 09:47:02 +0200

    Add OnClientReady plumbing for CosmosDB, MongoDB and Blob storage
    - Implement OnClientReady extensions that add resource annotations and OnResourceReady handlers
      to create SDK clients/containers and publish corresponding "*Ready" events.
    - Add event record types and annotation classes for BlobContainer, Mongo client/database and
      Cosmos DB client/database/container.
    - Wire subscription helper (OnEvent) to invoke user-provided OnClientReady callbacks.
    - Update AppHost sample to register OnClientReady handlers that log client/database/container
      readiness and perform example blob upload / Mongo insert.

--- ResourceClientReady/AppHost.cs
+++ ResourceClientReady/AppHost.cs
@@ -9,15 +9,31 @@ var builder = DistributedApplication.CreateBuilder(args);
 var cosmos = builder.AddAzureCosmosDB("cosmos-db")
     .RunAsEmulator(emulator => emulator.WithLifetime(ContainerLifetime.Persistent));
 
+cosmos.OnClientReady(async (evt, ct) =>
+{
+    var client = evt.Client;
+    Console.WriteLine($"CosmosDB Client is ready - account endpoint: {client.Endpoint}");
+});
+
 var cosmosDatabase = cosmos.AddCosmosDatabase("my-database");
+cosmosDatabase.OnClientReady(async (evt, ct) =>
+{
+    var database = evt.Database;
+    Console.WriteLine($"CosmosDB Database is ready - id: {database.Id}");
+});
 
 var cosmosContainer = cosmosDatabase.AddContainer("my-container", "/partitionKey");
+cosmosContainer.OnClientReady(async (evt, ct) =>
+{
+    var container = evt.Container;
+    Console.WriteLine($"CosmosDB Container is ready - id: {container.Id}");
+});
 
 var mongo = builder.AddMongoDB("mongo-db");
 mongo.OnClientReady(async (evt, ct) =>
 {
     var client = evt.Client;
-    Console.WriteLine($"MongoDB Client is ready - cluster: {client.Cluster.Description.ClusterId}");    
+    Console.WriteLine($"MongoDB Client is ready - cluster: {client.Cluster.Description.ClusterId}");
 });
 
 var mongoDatabase = mongo.AddDatabase("my-mongo-database");
@@ -47,13 +63,32 @@ public static class AzureStorageExtensions
 {
     public static IResourceBuilder<AzureBlobStorageResource> OnClientReady(this IResourceBuilder<AzureBlobStorageResource> builder,
         Func<BlobContainerClientReadyEvent, CancellationToken, Task> handler)
-        => builder.OnEvent<AzureBlobStorageResource, BlobContainerClientReadyEvent>((resource, evt, ct) => handler(evt, ct));
+    {
+        if (!builder.Resource.TryGetLastAnnotation<BlobContainerClientReadyAnnotation>(out var existingEvent))
+        {
+            builder.WithAnnotation(new BlobContainerClientReadyAnnotation());
+            builder.OnResourceReady(async (resource, evt, ct) =>
+            {
+                var containerClient = new BlobContainerClient(
+                    await resource.ConnectionStringExpression.GetValueAsync(ct),
+                    resource.Name);
+
+                await containerClient.CreateIfNotExistsAsync(cancellationToken: ct);
+
+                var readyEvent = new BlobContainerClientReadyEvent(resource, containerClient);
+                await builder.ApplicationBuilder.Eventing.PublishAsync(readyEvent, ct);
+            });
+        }
+        return builder.OnEvent<AzureBlobStorageResource, BlobContainerClientReadyEvent>((resource, evt, ct) => handler(evt, ct));
+    }
 
     private static IResourceBuilder<TResource> OnEvent<TResource, TEvent>(this IResourceBuilder<TResource> builder, Func<TResource, TEvent, CancellationToken, Task> callback) where TResource : IResource where TEvent : IDistributedApplicationResourceEvent
     {
         builder.ApplicationBuilder.Eventing.Subscribe(builder.Resource, (TEvent evt, CancellationToken ct) => callback(builder.Resource, evt, ct));
         return builder;
     }
+
+    private class BlobContainerClientReadyAnnotation : IResourceAnnotation;
 }
 
 public record class BlobContainerClientReadyEvent(AzureBlobStorageResource Resource, BlobContainerClient Client) : IDistributedApplicationResourceEvent
@@ -72,7 +107,7 @@ public static class MongoDBHostingExtensions
             {
                 var connectionString = await resource.ConnectionStringExpression.GetValueAsync(ct);
                 IMongoClient client = new MongoClient(connectionString);
-                
+
                 var readyEvent = new MongoClientReadyEvent(resource, client);
                 await builder.ApplicationBuilder.Eventing.PublishAsync(readyEvent, ct);
             });
@@ -87,7 +122,24 @@ public static class MongoDBHostingExtensions
 
     public static IResourceBuilder<MongoDBDatabaseResource> OnClientReady(this IResourceBuilder<MongoDBDatabaseResource> builder, Func<MongoDatabaseReadyEvent, CancellationToken, Task> handler)
     {
+        if (!builder.Resource.TryGetLastAnnotation<MongoDatabaseReadyAnnotation>(out var existingEvent))
+        {
+            builder.WithAnnotation(new MongoDatabaseReadyAnnotation());
+            builder.OnResourceReady(async (resource, evt, ct) =>
+            {
+                var parentConnectionString = await resource.Parent.ConnectionStringExpression.GetValueAsync(ct);
+                IMongoClient client = new MongoClient(parentConnectionString);
+                IMongoDatabase database = client.GetDatabase(resource.Name);
+
+                var readyEvent = new MongoDatabaseReadyEvent(resource.Parent, database);
+                await builder.ApplicationBuilder.Eventing.PublishAsync(readyEvent, ct);
+            });
+        }
         return builder.OnEvent((MongoDBDatabaseResource resource, MongoDatabaseReadyEvent evt, CancellationToken ct) => handler(evt, ct));
+    }
+
+    private class MongoDatabaseReadyAnnotation : IResourceAnnotation
+    {
     }
 
     private static IResourceBuilder<TResource> OnEvent<TResource, TEvent>(this IResourceBuilder<TResource> builder, Func<TResource, TEvent, CancellationToken, Task> callback) where TResource : IResource where TEvent : IDistributedApplicationResourceEvent
@@ -103,6 +155,90 @@ public record class MongoClientReadyEvent(MongoDBServerResource Resource, IMongo
 }
 
 public record class MongoDatabaseReadyEvent(MongoDBServerResource Resource, IMongoDatabase Database) : IDistributedApplicationResourceEvent
+{
+    IResource IDistributedApplicationResourceEvent.Resource => Resource;
+}
+
+public static class AzureCosmosExtensions
+{
+    public static IResourceBuilder<AzureCosmosDBResource> OnClientReady(this IResourceBuilder<AzureCosmosDBResource> builder, Func<CosmosDBClientReadyEvent, CancellationToken, Task> handler)
+    {
+        if (!builder.Resource.TryGetLastAnnotation<CosmosDBClientReadyAnnotation>(out var existingEvent))
+        {
+            builder.WithAnnotation(new CosmosDBClientReadyAnnotation());
+            builder.OnResourceReady(async (resource, evt, ct) =>
+            {
+                var connectionString = await resource.ConnectionStringExpression.GetValueAsync(ct);
+                var client = new Microsoft.Azure.Cosmos.CosmosClient(connectionString);
+
+                var readyEvent = new CosmosDBClientReadyEvent(resource, client);
+                await builder.ApplicationBuilder.Eventing.PublishAsync(readyEvent, ct);
+            });
+        }
+        return builder.OnEvent((AzureCosmosDBResource resource, CosmosDBClientReadyEvent evt, CancellationToken ct) => handler(evt, ct));
+    }
+
+    // for database
+    public static IResourceBuilder<AzureCosmosDBDatabaseResource> OnClientReady(this IResourceBuilder<AzureCosmosDBDatabaseResource> builder, Func<CosmosDatabaseReadyEvent, CancellationToken, Task> handler)
+    {
+        if (!builder.Resource.TryGetLastAnnotation<CosmosDBClientReadyAnnotation>(out var existingEvent))
+        {
+            builder.WithAnnotation(new CosmosDBClientReadyAnnotation());
+            builder.OnResourceReady(async (resource, evt, ct) =>
+            {
+                var connectionString = await resource.Parent.ConnectionStringExpression.GetValueAsync(ct);
+                var cosmosClient = new Microsoft.Azure.Cosmos.CosmosClient(connectionString);
+                var database = cosmosClient.GetDatabase(resource.Name);
+
+                var readyEvent = new CosmosDatabaseReadyEvent(resource.Parent, database);
+                await builder.ApplicationBuilder.Eventing.PublishAsync(readyEvent, ct);
+            });
+        }
+        return builder.OnEvent((AzureCosmosDBDatabaseResource resource, CosmosDatabaseReadyEvent evt, CancellationToken ct) => handler(evt, ct));
+    }
+
+    // for container
+    public static IResourceBuilder<AzureCosmosDBContainerResource> OnClientReady(this IResourceBuilder<AzureCosmosDBContainerResource> builder, Func<CosmosContainerReadyEvent, CancellationToken, Task> handler)
+    {
+        if (!builder.Resource.TryGetLastAnnotation<CosmosDBClientReadyAnnotation>(out var existingEvent))
+        {
+            builder.WithAnnotation(new CosmosDBClientReadyAnnotation());
+            builder.OnResourceReady(async (resource, evt, ct) =>
+            {
+                var connectionString = await resource.Parent.Parent.ConnectionStringExpression.GetValueAsync(ct);
+                var cosmosClient = new Microsoft.Azure.Cosmos.CosmosClient(connectionString);
+                var database = cosmosClient.GetDatabase(resource.Parent.Name);
+                var container = database.GetContainer(resource.Name);
+
+                var readyEvent = new CosmosContainerReadyEvent(resource, container);
+                await builder.ApplicationBuilder.Eventing.PublishAsync(readyEvent, ct);
+            });
+        }
+        return builder.OnEvent((AzureCosmosDBContainerResource resource, CosmosContainerReadyEvent evt, CancellationToken ct) => handler(evt, ct));
+    }
+
+    private class CosmosDBClientReadyAnnotation : IResourceAnnotation
+    {
+    }
+
+    private static IResourceBuilder<TResource> OnEvent<TResource, TEvent>(this IResourceBuilder<TResource> builder, Func<TResource, TEvent, CancellationToken, Task> callback) where TResource : IResource where TEvent : IDistributedApplicationResourceEvent
+    {
+        builder.ApplicationBuilder.Eventing.Subscribe(builder.Resource, (TEvent evt, CancellationToken ct) => callback(builder.Resource, evt, ct));
+        return builder;
+    }
+}
+
+public record class CosmosDBClientReadyEvent(AzureCosmosDBResource Resource, Microsoft.Azure.Cosmos.CosmosClient Client) : IDistributedApplicationResourceEvent
+{
+    IResource IDistributedApplicationResourceEvent.Resource => Resource;
+}
+
+public record class CosmosDatabaseReadyEvent(AzureCosmosDBResource Resource, Microsoft.Azure.Cosmos.Database Database) : IDistributedApplicationResourceEvent
+{
+    IResource IDistributedApplicationResourceEvent.Resource => Resource;
+}
+
+public record class CosmosContainerReadyEvent(AzureCosmosDBContainerResource Resource, Microsoft.Azure.Cosmos.Container Container) : IDistributedApplicationResourceEvent
 {
     IResource IDistributedApplicationResourceEvent.Resource => Resource;
 }
